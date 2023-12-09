bc90c074e55d86db5e04f9486b08b361fa5f315f
CormacDaly <cormacdaly24@gmail.com>
2023-12-09 13:16:55 +0000

    lib/solmate updated, removed ExitVault from deploy script

--- script/DeployContracts.s.sol
+++ script/DeployContracts.s.sol
@@ -5,15 +5,16 @@ pragma solidity 0.8.20;
 import {Script, console} from "forge-std/Script.sol";
 import {MockCCIPBnMToken, MockTestToken, MockLinkToken, MockDestinationVault} from "test/dummy-tokens/TestTokens.sol";
 import {SourceVault} from "src/SourceVault.sol";
-import {ExitVault} from "src/ExitVault.sol";
 import {ERC20} from "lib/solmate/src/tokens/ERC20.sol";
+
+// import {ExitVault} from "src/ExitVault.sol";
 
 contract DeployContracts is Script {
     MockCCIPBnMToken public mockCCIPBnM;
     MockTestToken public mockTest;
     MockLinkToken public mockLink;
     SourceVault public sourceVault;
-    ExitVault public exitVault;
+    // ExitVault public exitVault;
     MockDestinationVault public mockDestinationVault;
 
     function run() external {
@@ -31,8 +32,8 @@ contract DeployContracts is Script {
             address(0x23618e81E3f5cdF7f54C3d65f7FBc0aBf5B21E8f), // Dummy router address Anvil 8
             address(mockLink) // Dummy Link Token
         );
-        exitVault = new ExitVault();
-        exitVault.setSourceVault(address(sourceVault));
+        // exitVault = new ExitVault();
+        // exitVault.setSourceVault(address(sourceVault));
 
         mockDestinationVault = new MockDestinationVault(address(mockCCIPBnM));
         mockDestinationVault.setSourceVault(address(sourceVault));
